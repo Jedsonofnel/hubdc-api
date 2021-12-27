7242e1d45f3025eb0856aa94a90c1ca4bc6271a5
Jedn <jed@nelson.ac>
2021-12-27 10:38:18 +0000

    Add POST and PUT methods

--- data/events.go
+++ data/events.go
@@ -1,8 +1,9 @@
 package data
 
 import (
-    "encoding/json"
-    "io"
+	"encoding/json"
+	"fmt"
+	"io"
 )
 
 type Event struct {
@@ -19,8 +20,47 @@ func (e *Events) ToJSON(w io.Writer) error {
     return enc.Encode(e)
 }
 
+func (e *Event) FromJSON(r io.Reader) error {
+    d := json.NewDecoder(r)
+    return d.Decode(e)
+}
+
 func GetEvents() Events {
     return eventList
+}
+
+func AddEvent(e *Event) {
+    e.ID = getNextID()
+    eventList = append(eventList, e)
+}
+
+func UpdateEvent(id int, e *Event) error {
+    _, pos, err := findEvent(id)
+    if err != nil {
+        return err
+    }
+
+    e.ID = id
+    eventList[pos] = e
+
+    return nil
+}
+
+var ErrEventNotFound = fmt.Errorf("Event not found")
+
+func findEvent(id int) (*Event, int, error) {
+    for i, e := range eventList {
+        if e.ID == id {
+            return e, i, nil
+        }
+    }
+
+    return nil, -1, ErrEventNotFound
+}
+
+func getNextID() int {
+    le := eventList[len(eventList)-1]
+    return le.ID + 1
 }
 
 var eventList = []*Event{
--- handlers/events.go
+++ handlers/events.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
-	"github.com/Jedsonofnel/hubdc-api/data"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
+
+	"github.com/Jedsonofnel/hubdc-api/data"
 )
 
 type Events struct {
@@ -20,6 +23,37 @@ func (e *Events) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
         return
     }
 
+    if r.Method == http.MethodPost {
+        e.addEvent(rw, r)
+        return
+    }
+
+    if r.Method == http.MethodPut {
+        e.l.Println("PUT")
+        // extract id from url path
+        reg := regexp.MustCompile(`/([0-9]+)`)
+        g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+        if len(g) != 1 {
+            http.Error(rw, "Invalid URL", http.StatusBadRequest)
+            return
+        }
+
+        if len(g[0]) != 2 {
+            http.Error(rw, "Invalid URL", http.StatusBadRequest)
+            return
+        }
+        idString := g[0][1]
+        id, err := strconv.Atoi(idString)
+        if err != nil {
+            http.Error(rw, "Invalid URL", http.StatusBadRequest)
+            return
+        }
+
+        e.updateEvents(id, rw, r)
+        return
+    }
+
     rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
@@ -29,4 +63,39 @@ func (e *Events) getEvents(rw http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(rw, "Unable to marshall json", http.StatusInternalServerError)
 	}
+}
+
+func (e *Events) addEvent(rw http.ResponseWriter, r *http.Request) {
+    e.l.Println("Handle POST Event")
+
+    event := &data.Event{}
+
+    err := event.FromJSON(r.Body)
+    if err != nil {
+        http.Error(rw, "unable to unmarshal json", http.StatusBadRequest)
+    }
+
+    data.AddEvent(event)
+}
+
+func (e *Events) updateEvents (id int, rw http.ResponseWriter, r *http.Request) {
+    e.l.Println("Handle PUT Event")
+
+    event := &data.Event{}
+
+    err := event.FromJSON(r.Body)
+    if err != nil {
+        http.Error(rw, "unable to unmarhsal json", http.StatusBadRequest)
+    }
+
+    err = data.UpdateEvent(id, event)
+    if err == data.ErrEventNotFound {
+        http.Error(rw, "Event not found", http.StatusNotFound)
+        return
+    }
+
+    if err != nil {
+        http.Error(rw, "Event not found", http.StatusInternalServerError)
+        return
+    }
 }
